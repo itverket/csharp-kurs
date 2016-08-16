ac97d02efc0f5f90c3939a7d194f6d1f8febf91b
Anders Kofoed <anders.kfd@gmail.com>
2016-08-16 20:22:28 +0200

    Initial commit, added dummy test

---
+++ CSharpKurs/CSharpKurs.Tests/Task1Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace CSharpKurs.Tests
+{
+    [TestFixture]
+    public class Task1Tests
+    {
+
+        private readonly Task1 _task1 = new Task1();
+
+
+        [Test]
+        public void Add()
+        {
+            _task1.Add(1, 2).Should().Be(3);
+        }
+    }
+}
---
+++ CSharpKurs/CSharpKurs/Task1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpKurs
+{
+    public class Task1
+    {
+
+        public int? Add(int a, int b)
+        {
+            //return a + b;
+
+            return null;
+        }
+    }
+}
