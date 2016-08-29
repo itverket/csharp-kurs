12ae9abb3493bf47a52d1bffc727f19c1c820252
Magne Ingvaldsen <mi@itverket.no>
2016-08-29 22:38:50 +0200

    Added empty core project with nunit

---
+++ test/CSharpKurs.Test/Test1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace CSharpKurs.Tests
+{
+    [TestFixture]
+    public class Test1
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.IsTrue(Class1.ReturnsTrue());
+        }
+    }
+}
