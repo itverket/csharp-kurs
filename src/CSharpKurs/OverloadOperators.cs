bf2d9e7a5a6964d9f83b34645bbfbb094022f0a5
Anders Kofoed <anders.kfd@gmail.com>
2016-08-30 20:56:28 +0200

    Add overloadOperator task

---
+++ src/CSharpKurs/OverloadOperators.cs
@@ -0,0 +1,28 @@
+﻿namespace CSharpKurs
+{
+    public class OverloadOperators
+    {
+        
+         
+    }
+
+
+    /*
+    Implement an addition operator +, which adds to planks. Two planks are
+    added by returning a new plank with Length equal to the sum of the two previouse.
+     */
+    public class Plank
+    {
+        public int Length { get; set; }
+
+        public Plank(int length)
+        {
+            Length = length;
+        }
+
+        //public static Plank operator +(Plank plank1, Plank plank2)
+        //{
+        //    return new Plank(plank1.Length + plank2.Length);
+        //}
+    }
+}
---
+++ test/CSharpKurs.Test/OverloadOperatorTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Compatibility;
+using NUnit.Framework;
+
+namespace CSharpKurs.Tests
+{
+    [TestFixture]
+    public class OverloadOperatorTests
+    {
+
+        [Test]
+        public void Should_add_the_length_of_the_planks()
+        {
+            var plank1 = new Plank(1);
+            var plank2 = new Plank(5);
+
+            if (typeof(Plank).GetMethod("op_Addition") != null)
+            {
+                var resultPlank = typeof(Plank).GetMethod("op_Addition").Invoke(null, new Plank[] { plank1, plank2 });
+                Assert.That(((Plank)resultPlank).Length, Is.EqualTo(6));
+            }
+            else
+            {
+                Assert.Fail();
+            }
+
+
+
+        }
+    }
+}
