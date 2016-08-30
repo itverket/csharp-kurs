e5ca89a8b773341d471de1f77af1a84be77a3dcd
Anders Kofoed <anders.kfd@gmail.com>
2016-08-30 22:01:50 +0200

    Create OverloadOperator task and started polymorphism task

---
+++ src/CSharpKurs/Polymorphism.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpKurs
+{
+    /*
+    Alter the Line and Circle classes so that it returns:  "Line,Circle"
+    */
+    public class DrawingObject
+    {
+        public virtual string Draw()
+        {
+            return "Empty";
+        }
+    }
+
+    public class Line : DrawingObject
+    {
+        public override string Draw()
+        {
+            return "Line";
+        }
+    }
+
+    public class Circle : DrawingObject
+    {
+        public override string Draw()
+        {
+            return "Circle";
+        }
+
+    }
+
+
+
+}
---
+++ test/CSharpKurs.Test/PolymorphismTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CSharpKurs.Tests
+{
+    [TestFixture]
+    public class PolymorphismTests
+    {
+
+        [Test]
+        public void Should_print_correct_string_using_overloads()
+        {
+
+        }
+
+
+    }
+}
