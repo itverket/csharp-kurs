b24bf8291da314b9b8ff11fd5ca334d06e53daa2
Ole Andreas <orydland@gmail.com>
2016-08-31 23:09:09 +0200

    FIrst simple generic task

---
+++ src/CSharpKurs/Generics.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpKurs
+{
+    /* 
+     * Now we will attempt to solve some exercises where we will have to work with generics. This should be fun!
+     */
+    public class FunWithGenerics
+    {
+        /* 
+         * The first task is to make the class MyVeryOwnList take a generic parameter
+         */
+
+            //Remove <T> to make test fail
+        public class MyVeryOwnList<T>
+        {
+            
+        }
+
+
+    }
+}
---
+++ test/CSharpKurs.Test/GenericTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using static CSharpKurs.FunWithGenerics;
+
+namespace CSharpKurs.Tests
+{
+    [TestFixture]
+    public class FunWithGenericsTests
+    {
+        private FunWithGenerics _myVeryOwnList = new FunWithGenerics();
+
+        [Test]
+        public void MyVeryOwnListSouldTakeGenericParameter()
+        {
+            var myVeryOwnList = new MyVeryOwnList<int>();
+            var genericTypeArguments = myVeryOwnList.GetType().GenericTypeArguments.Length;
+
+            Assert.AreEqual(1, genericTypeArguments);
+        }
+    }
+}
