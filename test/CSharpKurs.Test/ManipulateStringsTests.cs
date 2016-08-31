0c18edb64070c191aa83752f593461eec28a62f6
Ole Andreas <orydland@gmail.com>
2016-08-31 22:46:07 +0200

    added simple string manipulation tasks and tests. Can easily add more simple introductionary coding stuff here if we need.

---
+++ test/CSharpKurs.Test/ManipulateStringsTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace CSharpKurs.Tests
+{
+    [TestFixture]
+    public class ManipulateStringsTests
+    {
+        private ManipulateStrings _manipulateStrings = new ManipulateStrings();
+
+        [Test]
+        public void PlusOperatorConcatStrings()
+        {
+            var result = _manipulateStrings.PlusOperatorConcatStrings();
+
+            Assert.AreEqual("Hello World", result);
+        }
+
+        [Test]
+        public void FormatString()
+        {
+            var result = _manipulateStrings.StringFormat();
+
+            Assert.AreEqual("Hello to you too World", result);
+        }
+
+        [Test]
+        public void StringInterpolation()
+        {
+            var result = _manipulateStrings.StringInterpolation();
+
+            Assert.AreEqual("Hello to you too World", result);
+        }
+
+        [TestCase("", "Empty string")]
+        [TestCase(null, "Empty string")]
+        [TestCase("a", "Hello World")]
+        [TestCase("Hello World", "Hello World")]
+        public void CheckForNullOrEmpty(string input, string expected)
+        {
+            var result = _manipulateStrings.CheckForNullOrEmpty(input);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
