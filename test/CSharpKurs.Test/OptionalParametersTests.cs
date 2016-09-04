9467da9220b70c5eb01a8fee7802a46245c29172
Magne Ingvaldsen <mi@itverket.no>
2016-09-04 21:59:28 +0200

    Removed the passing tests

--- test/CSharpKurs.Test/OptionalParametersTests.cs
+++ test/CSharpKurs.Test/OptionalParametersTests.cs
@@ -8,34 +8,15 @@ namespace CSharpKurs.Tests
     public class OptionalParametersTests
     {
         [Test]
-        public void Ctor_should_set_private_field()
-        {
-            var optionalParameters = new OptionalParameters(1337);
-            var field = (int)optionalParameters.GetType().GetField("_myNumber",
-                         BindingFlags.NonPublic |
-                         BindingFlags.Instance).GetValue(optionalParameters);
-
-            Assert.AreEqual(1337, field);            
-        }
-
-        [Test]
         public void Ctor_should_have_optional_parameter()
         {
             var type = typeof(OptionalParameters);
             var ctor = type.GetConstructor(new[] { typeof(int) });
-            var instance = ctor.Invoke(new object[] {Type.Missing}) as OptionalParameters;
+            var instance = ctor.Invoke(new object[] { Type.Missing }) as OptionalParameters;
             var field = (int)type.GetField("_myNumber",
                         BindingFlags.NonPublic |
                         BindingFlags.Instance).GetValue(instance);
             Assert.AreEqual(0, field);
-        }
-
-        [Test]
-        public void OptionalStringParameter_should_use_both_parameters()
-        {
-            var optionalParameters = new OptionalParameters(1337);
-            var greeting = optionalParameters.OptionalStringParameter("Per", "Person");
-            Assert.AreEqual("Hello Per Person", greeting);
         }
 
         [Test]
@@ -45,14 +26,6 @@ namespace CSharpKurs.Tests
             var method = optionalParameters.GetType().GetMethod("OptionalStringParameter");
             var result = method.Invoke(optionalParameters, new object[] { "Ole", Type.Missing });
             Assert.AreEqual("Hello Ole Olsen", result);
-        }
-
-        [Test]
-        public void CanFirstParameterBeOptional_should_use_both_parameters()
-        {
-            var optionalParameters = new OptionalParameters(1337);
-            var greeting = optionalParameters.CanFirstParameterBeOptional("Per", "Person");
-            Assert.AreEqual("Hello Per Person", greeting);
         }
 
         [Test]
