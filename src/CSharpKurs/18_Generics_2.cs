c3f8972a5579c895e4098e6c2d82ef6751aa6c67
Rydland, Ole Andreas <ole.andreas.rydland@if.no>
2016-09-05 09:02:05 +0200

    everything except generics_4 compiles without code body

---
+++ src/CSharpKurs/18_Generics_2.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharpKurs
+{
+    public class FunWithGenerics2
+    {
+        /* 
+         * Now add a public property that is an array of the generic type parameter called 'List' to the class. We will use this array
+         * to hold the elements of our list
+         */
+        public class MyVeryOwnListWithGenericArray<T>
+        {
+            public T[] List { get; set; }
+        }
+    }
+}
---
+++ src/CSharpKurs/18_Generics_4.cs
@@ -0,0 +1,58 @@
+﻿namespace CSharpKurs
+{
+    public class FunWithGenerics4
+    {
+        /* 
+        * In this next sections we will work with what we call constraint parameters. THis is a way of saying that our generic parameter has to implement some specific functionality to be usable.
+        * In this case we wish to add a SumAll method to our generic class. However, we can't be sure that all types of objects can be summed, therefore we have to introduce a constraint!
+        */
+
+        /* 
+         * First, create an interface called ISummable that has one method called Sum that takes no parameters and returns an int.
+         */
+
+        public interface ISummable
+        {
+            int Sum();
+        }
+
+        /* 
+         * Now add a SumAll() method to the generic class we made earlier. You will see that this leads to a compilation error. Try to add a type constraint to the class to fix this.
+         * HINT: You add a type constraint with the "where T : " syntax
+         */
+
+        public class MyVeryOwnListWithSumAll<T> where T : ISummable
+        {
+            public T[] list { get; set; }
+
+            public T[] Add(T addThis)
+            {
+                if (list == null)
+                {
+                    return new T[] {addThis};
+                }
+                var temp = list;
+                list = new T[temp.Length + 1];
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    list[i] = temp[i];
+                }
+                list[temp.Length] = addThis;
+
+                return list;
+            }
+
+            public int SumAll()
+            {
+                var sum = 0;
+
+                foreach (var item in list)
+                {
+                    sum += item.Sum();
+                }
+
+                return sum;
+            }
+        }
+    }
+}
