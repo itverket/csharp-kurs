5d63dda27b7cb07a0059476aae403fb76a0f1def
Anders Kofoed <anders.kfd@gmail.com>
2017-03-19 15:43:45 +0100

    Add BlackJack exercise to the solution

---
+++ BlackJack.Tests/Given_Deck/When_initialized.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace BlackJack.Tests.Given_Deck
+{
+    [TestClass]
+    public class When_initialized : Scenario
+    {
+        private Deck _deck;
+        public override void When()
+        {
+            _deck = new Deck();
+        }
+
+        [Then]
+        public void Should_have_52_cards()
+        {
+            Assert.AreEqual(52, _deck.Cards.Count);
+        }
+
+        [Then]
+        public void Should_have_4_distinct_suits()
+        {
+            Assert.AreEqual(4, _deck.Cards.Select(x => (int)x.Suit).Distinct().ToList().Count);
+        }
+    }
+}
---
+++ BlackJack.Tests/Scenario.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlackJack.Tests
+{
+    public abstract class Scenario
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            Given();
+            When();
+        }
+
+        public virtual void Given() { }
+
+        public virtual void When() { }
+
+
+        public class ThenAttribute : TestMethodAttribute
+        {
+
+        }
+
+    }
+
+}
